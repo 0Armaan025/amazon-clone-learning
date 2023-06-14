88815b9aa29aadb4215901c290d84915eabab3a0
0Armaan025 <armaan33000@gmail.com>
2023-06-14 17:40:52 +0530

    made an api

--- server/index.js
+++ server/index.js
@@ -1,1 +1,19 @@
-console.log("hello, world!")
+// console.log("hello, world!")
+const express = require('express');
+
+const app = express();
+
+//CREATING AN API
+app.get("/hello-world",(req, res) => {
+    res.send("hey there armaan")
+});
+//GET, PUT , POST, DELETE, UPDATE -> CRUD
+
+// Create, Read, Update, Delete
+
+
+const PORT = 3000;
+
+app.listen(PORT, "0.0.0.0", () => {
+    console.log(`connected at port ${PORT}`)
+});
